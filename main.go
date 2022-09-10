80115fc22d5709cf63b95885ed4748d07c4654a7
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-09-11 02:56:12 +0900

    Fix sample data

--- main.go
+++ main.go
@@ -8,21 +8,18 @@ import (
 )
 
 func main() {
-	// You can use the Parameters structure to set the parameters
-	p := sunrisesunset.Parameters{
-		Latitude:  -23.545570,
-		Longitude: -46.704082,
-		UtcOffset: -3.0,
-		Date:      time.Date(2017, 3, 23, 0, 0, 0, 0, time.UTC),
+	params := sunrisesunset.Parameters{
+		Latitude:  35.70012,
+		Longitude: 139.79013,
+		UtcOffset: +9.0,
+		Date:      time.Date(2022, 9, 13, 0, 0, 0, 0, time.UTC),
 	}
 
-	// Calculate the sunrise and sunset times
-	sunrise, sunset, err := p.GetSunriseSunset()
+	sunrise, sunset, err := params.GetSunriseSunset()
 
-	// If no error has occurred, print the results
 	if err == nil {
-		fmt.Println("Sunrise:", sunrise.Format("15:04:05")) // Sunrise: 06:11:44
-		fmt.Println("Sunset:", sunset.Format("15:04:05"))   // Sunset: 18:14:27
+		fmt.Println("Sunrise:", sunrise.Format("15:04:05"))
+		fmt.Println("Sunset:", sunset.Format("15:04:05"))
 	} else {
 		fmt.Println(err)
 	}
